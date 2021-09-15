09f03b2081817a1a2a16c13548996306ff879b2b
Gabriel Ferreira <64704484+Harlock221B@users.noreply.github.com>
2021-09-15 09:43:16 -0300

    First Commit

--- home/models.py
+++ home/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.utils import timezone
 import main
 
+class Produtora(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=255, blank=True)
+    logo_photo = models.ImageField(blank=True, upload_to='logos_empresas/%y/%m/%d/')
+
+    def __str__(self):
+                return self.name
+
 class Games(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
+    lancamento = models.DateTimeField(default=timezone.now())
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=250)
+    produtora = models.ForeignKey(Produtora, on_delete=models.DO_NOTHING)
+    photo = models.ImageField(blank=True, upload_to='fotos/%y/%m/%d/')
+    
+    def __str__(self):
+                return self.name
 
 def inserir_dados(self):
-    conexao.child('vendas').push(games)
+    conexao.child('games').push(games)
 
 def deletar_dados(self):
-    conexao.child('vendas').delete(games)
+    conexao.child('games').delete(games)
 
 def editar_dados(self):
-    conexao.child('vendas').update(games)
+    conexao.child('games').update(games)
 
 def exibir_dados(self):
     dados = conexao.child('vendas').get()
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404, get_object_or_404
+from django.db.models import Q
+from .models import Games
+
+def index(request): 
+    dados = Games.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    return render(request,'home/index.html',{'dados':dados})
 
 # Create your views here.
